f8f39e16ed3ac1f715b5e8eb9666724ad0a31a8f
lorenalferraz <lorenalferraz@gmail.com>
2025-11-04 08:15:34 -0300

    Deploy widget files for GitHub Pages

--- src/runtime/translations/default.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  _widgetLabel: 'Add Layers',
-  addLayer: 'Add Layer',
-  featureServiceUrl: 'Feature Service URL',
-  instructions:
-    'Copie a FeatureService Layer URL here to add it to the map.'
-}
