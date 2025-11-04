58e8ae329cd99ea49213180e3a1388c3f454b1f2
lorenalferraz <lorenalferraz@gmail.com>
2025-11-04 13:08:03 -0300

    Limpar dist recursivo e atualizar manifest/translations com nome correto do widget

--- dist/runtime/translations/default.ts
+++ dist/runtime/translations/default.ts
@@ -1,9 +1,10 @@
 export default {
-  _widgetLabel: 'Filtrar e Baixar',
+  _widgetLabel: 'Filtrar e Gerar Relatório',
   addLayer: 'Adicionar Camada',
   featureServiceUrl: 'URL do Feature Service',
   instructions:
-    'Digite um número na coluna "idea" para filtrar e dar zoom no polígono correspondente.',
+    'Digite o nº IDEA',
+  generateReport: 'Gerar Relatório',
   searchIdea: 'Pesquisar por número (idea)',
   filterAndZoom: 'Filtrar',
   clearFilter: 'Limpar Filtro',
